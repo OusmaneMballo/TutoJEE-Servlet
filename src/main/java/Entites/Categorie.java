2562d41b3d1111f881bf75b1eafb501671a6a6a6
OusmaneMballo <ousmanemballo576@gmail.com>
2020-10-13 10:39:12 +0000

    Ajout du fichier conf.xml

--- src/main/java/Entites/Categorie.java
+++ src/main/java/Entites/Categorie.java
@@ -1,4 +1,35 @@
 package Entites;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
 public class Categorie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 30, unique = true, nullable = false)
+    private String libelle;
+    @OneToMany(mappedBy = "categorie")
+    private List<Produit> produits;
+
+    public Categorie() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
 }
--- src/main/java/Entites/Produit.java
+++ src/main/java/Entites/Produit.java
@@ -1,4 +1,68 @@
 package Entites;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
 public class Produit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 30)
+    private String libelle;
+
+    @Column(nullable = false)
+    private float prixU;
+    @Column(nullable = false)
+    private int quantite;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "categorie_id")
+    private Categorie categorie;
+
+    public Produit() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public float getPrixU() {
+        return prixU;
+    }
+
+    public void setPrixU(float prixU) {
+        this.prixU = prixU;
+    }
+
+    public int getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite(int quantite) {
+        this.quantite = quantite;
+    }
+
+    public Categorie getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(Categorie categorie) {
+        this.categorie = categorie;
+    }
 }
